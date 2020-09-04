6c02e65045253fe4b88681c2137018e661bc25df
danil0110 <danilmail0110@gmail.com>
2020-09-04 14:53:55 +0300

    FileGeneration method implemented

--- Algorithms_LR1/Program.cs
+++ Algorithms_LR1/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Algorithms_LR1
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace Algorithms_LR1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
         }
+
+        public static void FileGeneration()
+        {
+            StreamWriter sw = new StreamWriter("data.txt");
+            Random rnd = new Random();
+            sw.Write(Convert.ToString(rnd.Next(-200, 200)));
+            while (true)
+            {
+                sw.Write(" " + Convert.ToString(rnd.Next(-200, 200)));
+            }
+        }
+        
     }
 }
